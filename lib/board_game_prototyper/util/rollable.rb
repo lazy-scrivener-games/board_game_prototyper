c87d0d167383473975844c06f9f8b535404d497e
crimsonknave <crimsonknave@gmail.com>
2023-10-14 18:25:08 -0400

    Rollable linting and tests

--- lib/board_game_prototyper/util/rollable.rb
+++ lib/board_game_prototyper/util/rollable.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+# This class is used to allow for an easier time inside the `compute` keyword
+# Each line inside a `compute` block evaluates against @source and then
+# @source is set to the result. This allows for example, chaining filter and map
+# It also provides some helper methods like `append` and `multiply` to assist in
+# cases where the desired behavior a method directly on the @source.
 class Rollable
   extend Forwardable
   def_delegators :@source, :to_s
@@ -7,16 +14,14 @@ class Rollable
 
   # TODO: Build out helper methods
   # Other helpers: divide, add, subtract
-  def multiply(x)
+  def multiply(num)
     @source = case @source
-              when Integer
-                @source * x
               when Array
-                @source.map { |value| value * x }
+                @source.map { |value| value * num }
               when Hash
-                @source.transform_values { |value| value * x }
+                @source.transform_values { |value| value * num }
               else
-                @source
+                @source * num
               end
   end
 
@@ -24,8 +29,11 @@ class Rollable
     @source = "#{@source}#{to_append}"
   end
 
-  def method_missing(m, *args, &)
-    @source = @source.send(m, *args, &)
+  def method_missing(method_name, *args, &)
+    @source = @source.send(method_name, *args, &)
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    @source.respond_to?(method_name, include_private)
   end
 end
-
